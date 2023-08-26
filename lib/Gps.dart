f3f12bd46e6b22c28f39d9c655a7fac11d4e5802
Classic <kumaravelnivas@gmail.com>
2023-08-26 15:27:07 +0530

    updated at 26.08.2023

---
+++ lib/Gps.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:geolocator/geolocator.dart';
+
+
+typedef PositionCallback = Function (Position position);
+
+class Gps{
+
+  late StreamSubscription<Position> _positionstream;
+
+  bool isAccessGranted(LocationPermission permission){
+    return permission==LocationPermission.whileInUse||permission==LocationPermission.always;
+  }
+  Future<bool>requestpermission()async{
+    LocationPermission permission=await Geolocator.checkPermission();
+    if(isAccessGranted(permission)){
+      return true;
+    }
+      permission = await Geolocator.requestPermission();
+    return isAccessGranted(permission);
+    }
+  Future<void> startPositionstream(Function(Position position) callback)async{
+  bool permissionGranted= await requestpermission();
+  if(!permissionGranted){
+    throw Exception("User did not Grant permission");
+  }
+  _positionstream= await Geolocator.getPositionStream(
+    locationSettings: LocationSettings(accuracy: LocationAccuracy.bestForNavigation)
+  ).listen((callback) );
+  }
+
+  Future<void> stoppositionStream()async{
+    await _positionstream.cancel();
+  }
+}
--- lib/displaymap.dart
+++ lib/displaymap.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+
 import 'mathutil.dart';
 
 
